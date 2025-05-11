18f7254819064c2f97dc9650a87b325a2fd93e1c
Mike Hardy <hardym30@gmail.com>
2025-05-11 17:48:33 -0500

    Add files via upload

--- src/components/AddFavorites.jsx
+++ src/components/AddFavorites.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const AddFavorites = () => {
     return (
         <>
+        <div className="favorite__icon">
             {/* <FontAwesomeIcon icon="faHeart" /> */}
+        </div>
         </>
     )
 };
